8aad7760a963daad8e3a62a9762f35fb772a7106
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2023-04-17 23:41:13 -0600

    Init project

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const { faker } = require('@faker-js/faker');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hi there!');
+});
+
+app.get('/users', (req, res) => {
+  const { limit, offset } = req.query;
+  if (limit && offset) {
+    res.json({
+      limit,
+      offset,
+    });
+  } else {
+    res.send('No hay limit ni offset');
+  }
+});
+
+app.get('/products', (req, res) => {
+  const products = [];
+  const { size } = req.query;
+  const limit = size || 10;
+  for (let i = 0; i < limit; i++) {
+    products.push({
+      id: faker.random.number,
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.imageUrl(),
+    });
+  }
+  res.json(products);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
